4499c5a883de81eac87fd6d6198ae2b6b2ac5ba3
Unknown <rodrigofranco30@gmail.com>
2020-05-17 15:24:51 -0300

    script inicial de criação de tabelas v0_0_1

---
+++ src/main/java/br/com/bnp/teste/FlywayApplication.java
@@ -0,0 +1,14 @@
+package br.com.bnp.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayApplication.class, args);
+	}
+
+}
